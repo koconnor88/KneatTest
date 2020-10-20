0af3404022ab986e1ac22e548a32d75e5c37eef9
koconnor88 <KOConnor55@gmail.com>
2020-10-20 11:08:51 +0100

    Update BaseFilterControl.cs

--- KneatTest/Framework/Components/ResulltsFilterControls/BaseFilterControl.cs
+++ KneatTest/Framework/Components/ResulltsFilterControls/BaseFilterControl.cs
@@ -9,12 +9,11 @@ namespace KneatTest.Framework.Components.ResulltsFilterControls
 {
     public class BaseFilterControl : BaseControl
     {
+        //Would make an abstract class BaseFilterControl<T> where T Filter pane which would include options
+        //This would allow to use more than Checkbox filters i.e slider for price
+        //For the purposes of this exercise having options here is enough
         public BaseFilterControl(IWebElement referenceElement) : base(referenceElement){}
-
-        //.filteroptions > a gives list of options
-        //have option control with Name and select/unselect and count
-        //make virtual on base to over write if needed
-
+        
         protected virtual IWebElement FilterTitleELement => WrappedElement.FindElement(By.ClassName("filtercategory-title"));
 
         public string FilterTitle => FilterTitleELement.Text;
